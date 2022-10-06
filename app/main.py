aebd13c969da3a07acba1a240c56db94d92b51e1
ttwkr <ttwkor12@gmail.com>
2022-10-06 23:30:02 +0900

    wip async await

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import time
+import asyncio
 from fastapi import FastAPI
 
 from app.config import settings
@@ -5,9 +7,26 @@ from app.config import settings
 app = FastAPI()
 
 
+async def count1(num: int):
+    print(num)
+    await asyncio.sleep(1)
+    print("sleep")
+    return num
+
+
+async def count2(num: int):
+    print(num)
+    await asyncio.sleep(1)
+    print("sleep2")
+    return num
+
+
 @app.get("/")
 async def root():
-
+    # await count1(1)
+    # await count2(2)
+    a = [count1(1), count2(2)]
+    c = await asyncio.gather(*a)
     return {
         "info": {
             "DB": settings.RDS_DB,
