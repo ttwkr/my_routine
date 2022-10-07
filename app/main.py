6a6ab4f31c51f8f255abcaef0be74b88d6de8c18
ttwkr <ttwkor12@gmail.com>
2022-10-07 23:26:54 +0900

    remove async test code

--- app/main.py
+++ app/main.py
@@ -8,33 +8,8 @@ from app.config import settings
 app = FastAPI()
 
 
-async def count1(num: int):
-    print(datetime.datetime.now())
-    print(num)
-    await asyncio.sleep(1)
-    print("sleep")
-    print(datetime.datetime.now())
-    return num
-
-
-async def count2(num: int):
-    print(datetime.datetime.now())
-    print(num)
-    await asyncio.sleep(1)
-    print("sleep2")
-    print(datetime.datetime.now())
-    return num
-
-
 @app.get("/")
-async def root():
-    # await count1(1)
-    # await count2(2)
-    a = [count1(1), count2(2)]
-    await asyncio.gather(*a)
-    # await count1(1)
-    # await count2(2)
-    print('end')
+def root():
     return {
         "info": {
             "DB": settings.RDS_DB,
