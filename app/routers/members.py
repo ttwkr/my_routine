db5140480737d1b5ba2169f3e0194015578aa8a8
ttwkr <ttwkor12@gmail.com>
2022-10-08 21:01:53 +0900

    add frame member login

--- app/routers/members.py
+++ app/routers/members.py
@@ -10,10 +10,34 @@ router = APIRouter()
 @router.get('/')
 async def get(db: Session = Depends(get_db)):
     members = db.query(Member).all()
-
     return members
 
 
 @router.post('/')
 async def save(db: Session = Depends(get_db)):
+    """
+    회원가입
+    :param db: session
+    :return:
+    """
+    pass
+
+
+@router.post('login/')
+async def login(db: Session = Depends(get_db)):
+    """
+    로그인
+    :param db:
+    :return:
+    """
+    pass
+
+
+@router.post('login-out/')
+async def login_out(db: Session = Depends(get_db)):
+    """
+    로그아웃
+    :param db: session
+    :return:
+    """
     pass
